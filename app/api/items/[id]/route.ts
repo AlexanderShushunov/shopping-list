7bec5f38288d92fb6e385b3a2c0bd277ff5f4964
Alexandr Shushunov <ashushunov@evolution.com>
2025-04-10 01:44:58 +0100

    Remove console.log

--- app/api/items/[id]/route.ts
+++ app/api/items/[id]/route.ts
@@ -11,7 +11,7 @@ export async function PUT(
 ) {
   try {
     const session = await getServerSession(authOptions)
-    console.log(session);
+
     if (!session?.user?.email) {
       return NextResponse.json({ error: 'Unauthorized' }, { status: 401 })
     }
